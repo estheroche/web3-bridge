23c56193ad760fd4709fa6cbafc43a1639c4caf0
estheroche <estheraladioche569@gmail.com>
2023-06-05 05:44:37 -0500

    setup slider with Javascript

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Happy to play")
+const images = document.querySelectorAll('.slider')
+let current = 0
+images.forEach((image, index) => {
+    image.style.left = `${index * 100}%`
+})
+
+function slide(){
+    if(current > images.length -1 ){
+        current = 0
+    }
+    if(current < 0){
+        current = 0
+    }
+    images.forEach((slide) => {
+        slide.style.transform = `translateX(-${current * 100}%)`
+    })
+}
+
+setInterval(() => {
+    current += 1
+    slide()
+}, 3000);
