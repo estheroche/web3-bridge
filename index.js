c2c0a9c52d55c6f5997ae732f329ab025e6c2918
estheroche <estheraladioche569@gmail.com>
2023-06-05 05:57:54 -0500

    feat: move with button

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const images = document.querySelectorAll('.slider')
+const prev = document.getElementById('prev')
+const next = document.getElementById('next')
 let current = 0
 images.forEach((image, index) => {
     image.style.left = `${index * 100}%`
@@ -11,6 +13,7 @@ function slide(){
     if(current < 0){
         current = 0
     }
+    console.log("best")
     images.forEach((slide) => {
         slide.style.transform = `translateX(-${current * 100}%)`
     })
@@ -20,3 +23,18 @@ setInterval(() => {
     current += 1
     slide()
 }, 3000);
+
+next.addEventListener(('click'), () => {
+    
+    if(current < images.length){
+        current = current + 1
+        slide()
+    }
+})
+
+prev.addEventListener(('click'), () => {
+    if(current > 0){
+        current = current - 1
+        slide()
+    }
+})
